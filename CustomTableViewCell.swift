9d24df1472cb556f624f2312a309ce850a8b5666
Ivan Klimenkov <klimenkov.4game@gmail.com>
2019-12-12 17:09:03 +0200

    Custom Table View Cell

---
+++ CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Иван Клименков on 12/12/2019.
+//  Copyright © 2019 Ivan klimenkov. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Иван Клименков on 12/12/2019.
+//  Copyright © 2019 Ivan klimenkov. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+
+}
