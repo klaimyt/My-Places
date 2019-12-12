645032dd29b1332c76ef98b2a4bc70e112a67ba5
Ivan Klimenkov <klimenkov.4game@gmail.com>
2019-12-12 12:45:06 +0200

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Иван Клименков on 12/12/2019.
+//  Copyright © 2019 Ivan klimenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
