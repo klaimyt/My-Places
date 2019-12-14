3f37e095fb6a4f6bc1ba925129c13abe0ee5da71
Ivan Klimenkov <klimenkov.4game@gmail.com>
2019-12-14 17:02:54 +0200

    add editing window and edit algoritms

--- MyPlaces/NewPlaceTableViewController.swift
+++ MyPlaces/NewPlaceTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class NewPlaceTableViewController: UITableViewController {
     
+    var currentPlace: Place?
+    
     var imageIsChanged = false
     
     @IBOutlet weak var placeImage: UIImageView!
@@ -20,13 +22,11 @@ class NewPlaceTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
-
+              
         tableView.tableFooterView = UIView()
         saveButton.isEnabled = false
         placeName.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        setupEditScreen()
     }
     
     // MARK: Table view delegate
@@ -61,6 +61,30 @@ class NewPlaceTableViewController: UITableViewController {
             view.endEditing(true)
         }
     }
+    
+    private func setupEditScreen() {
+        if currentPlace != nil {
+            guard let data = currentPlace?.imageData, let image = UIImage(data: data) else { return }
+            setupNavigationBar()
+            imageIsChanged = true
+            
+            placeImage.image = image
+            placeImage.contentMode = .scaleAspectFill
+            placeName.text = currentPlace?.name
+            placeLocation.text = currentPlace?.location
+            placeType.text = currentPlace?.type
+        }
+    }
+    
+    private func setupNavigationBar() {
+        if let topItem = navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        }
+        navigationItem.leftBarButtonItem = nil
+        title = currentPlace?.name
+        saveButton.isEnabled = true
+    }
+    
     @IBAction func cancelAction(_ sender: Any) {
         dismiss(animated: true)
     }
@@ -83,7 +107,7 @@ extension NewPlaceTableViewController: UITextFieldDelegate {
         }
     }
     
-    func saveNewPlace() {
+    func savePlace() {
        
         
         var image: UIImage?
@@ -96,7 +120,17 @@ extension NewPlaceTableViewController: UITextFieldDelegate {
         let imageData = image?.pngData()
         
         let newPlace = Place(name: placeName.text!, location: placeLocation.text, type: placeType.text, imageData: imageData)
+        
+        if currentPlace != nil {
+            try! realm.write {
+                currentPlace?.name = newPlace.name
+                currentPlace?.location = newPlace.location
+                currentPlace?.type = newPlace.type
+                currentPlace?.imageData = newPlace.imageData
+            }
+        } else {
         StorageManager.saveObject(newPlace)
+        }
     }
 }
 
